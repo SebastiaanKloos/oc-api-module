d4085b595a1c1b879c7432dddb58578d05eb69f9
Sebastiaan Kloos <sebastiaan@codecycler.com>
2019-10-19 12:52:37 +0200

    No need to define the model in the plugins model schema anymore

--- ServiceProvider.php
+++ ServiceProvider.php
@@ -2,7 +2,9 @@
 
 use App;
 use Config;
+use Api\Classes\SchemaManager;
 use Api\Classes\SchemaSourceProvider;
+use Nuwave\Lighthouse\Schema\Directives\BaseDirective;
 use October\Rain\Support\ModuleServiceProvider;
 
 class ServiceProvider extends ModuleServiceProvider
@@ -17,6 +19,7 @@ class ServiceProvider extends ModuleServiceProvider
 		$this->registerSingletons();
 
         App::make('October\Rain\Support\ClassLoader')->addDirectories(base_path('graphql'));
+        SchemaManager::instance()->registerModelNamespaces();
 	}
 	
 	public function boot() 
@@ -46,10 +49,6 @@ class ServiceProvider extends ModuleServiceProvider
 
 	public function registerSingletons()
     {
-        App::singleton('api.schema', function () {
-            return \Api\Classes\SchemaManager::instance();
-        });
-
         App::singleton(\Nuwave\Lighthouse\Schema\Source\SchemaSourceProvider::class, \Api\Classes\SchemaSourceProvider::class);
     }
 
--- classes/SchemaManager.php
+++ classes/SchemaManager.php
@@ -1,5 +1,8 @@
 <?php namespace Api\Classes;
 
+use Config;
+use System\Classes\PluginManager;
+
 class SchemaManager
 {
     use \October\Rain\Support\Traits\Singleton;
@@ -28,5 +31,20 @@ class SchemaManager
     public function getAll()
     {
         return $this->schemas;
+    }
+
+    public function registerModelNamespaces()
+    {
+        $namespaces = config('lighthouse.namespaces.models');
+
+        foreach (PluginManager::instance()->getPlugins() as $key => $plugin) {
+            if (method_exists($plugin, 'registerGraphQLModels')) {
+                $namespace = str_replace('.', '\\', $key) . '\\Models';
+
+                array_push($namespaces, $namespace);
+            }
+        }
+
+        Config::set('lighthouse.namespaces.models', $namespaces);
     }
 }
